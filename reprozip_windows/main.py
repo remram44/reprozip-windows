0e62a613881767ae529d75a8994c7b34da7db5ce
Remi Rampin <remi@rampin.org>
2021-06-29 16:31:12 -0400

    Make the CSV parser into a class

--- reprozip_windows/main.py
+++ reprozip_windows/main.py
@@ -4,6 +4,7 @@
 from collections import Counter
 import csv
 import os.path
+import pathlib
 import pkg_resources
 import pyuac
 import re
@@ -47,8 +48,9 @@ def main():
     ])
 
     # Read CSV
-    operations = read_trace('temp.csv')
-    for op in operations:
+    reader = ProcmonCSVReader()
+    reader.read_csv('temp.csv')
+    for op in reader.operations:
         print(op)
 
 
@@ -98,83 +100,115 @@ IGNORED_OPERATIONS = {
 }
 
 
-def read_trace(filename):
-    traced_processes = set()
-    operations = []
-    unknown_operations = Counter()
-    unknown_modes = Counter()
+class ProcmonCSVReader(object):
+    def __init__(self):
+        self.filename = None
+        self.row_number = None
 
-    def parse_access(access_modes):
-        access_modes = set(access_modes)
-        unknown = access_modes - {
+        self.operations = []
+        self.traced_processes = set()
+        self.unknown_operations = Counter()
+        self.unknown_access_modes = Counter()
+
+    def read_csv(self, file):
+        try:
+            # Accept a path, open the file
+            if isinstance(file, (str, bytes, pathlib.Path)):
+                with open(file, 'r', encoding='utf-8-sig') as fp:
+                    self.filename = str(file)
+                    return self._read_csv(fp)
+            else:
+                self.filename = repr(file)
+                return self._read_csv(file)
+        finally:
+            self.filename = None
+            self.row_number = None
+
+    def _read_csv(self, file):
+        # Open CSV
+        reader = csv.reader(file)
+        try:
+            header = next(reader)
+        except StopIteration:
+            raise ValueError("Empty trace file")
+        else:
+            if header != [
+                "Time of Day", "Process Name", "PID", "Operation", "Path",
+                "Result", "Detail",
+            ]:
+                raise ValueError("Wrong column names")
+
+        # Iterate on rows
+        for idx, row in enumerate(reader, 2):
+            self.row_number = idx
+            self.process_row(*row)
+
+        if self.unknown_operations:
+            print(
+                "\nUnknown operations:\n%s" % '\n'.join(
+                    '    %s (%d)' % p
+                    for p in self.unknown_operations.items()
+                ),
+                file=sys.stderr,
+            )
+        if self.unknown_access_modes:
+            print(
+                "\nUnknown access modes:\n%s" % '\n'.join(
+                    '    %s (%d)' % p
+                    for p in self.unknown_access_modes.items()
+                ),
+                file=sys.stderr,
+            )
+
+    def parse_access_mode(self, modes):
+        modes = set(modes)
+        unknown = modes - {
             'Execute/Traverse', 'Generic Read', 'Read Attributes',
             'Read Data/List Directory', 'Synchronize', 'Generic Write',
         }
         for mode in unknown:
-            unknown_modes[mode] += 1
-        if 'Generic Write' in access_modes:
+            self.unknown_access_modes[mode] += 1
+        if 'Generic Write' in modes:
             return 'write'
         else:
             return 'read'
 
-    with open(filename, 'r', encoding='utf-8-sig') as csvfile:
-        reader = csv.reader(csvfile)
+    def process_row(
+        self, time, procname, pid, operation, path, result, details,
+    ):
+        if (
+            not self.traced_processes and operation == 'Process Start'
+            and procname == os.path.basename(sys.executable)
+        ):
+            print("Tracing %s: %s" % (pid, procname), file=sys.stderr)
+            self.traced_processes.add(pid)
+        elif pid not in self.traced_processes:
+            return
 
-        for idx, row in enumerate(reader):
-            time, procname, pid, operation, path, result, details = row
-
-            if (
-                not traced_processes and operation == 'Process Start'
-                and procname == os.path.basename(sys.executable)
-            ):
-                print("Tracing %s: %s" % (pid, procname), file=sys.stderr)
-                traced_processes.add(pid)
-            elif pid not in traced_processes:
-                continue
-
-            if operation in IGNORED_OPERATIONS:
-                pass
-            elif operation == 'Load Image':
-                if result == 'SUCCESS':
-                    operations.append(('read', path))
-            elif operation == 'CreateFile':
-                if result == 'SUCCESS':
-                    info = parse_details(details)
-                    mode = parse_access(info['Desired Access'])
-                    operations.append((mode, path))
-            elif operation == 'RegOpenKey':
-                pass  # TODO
-            elif operation == 'Process Create':
-                m = re.match(r'^PID: ([0-9]+), Command line: .*$', details)
-                if m is None:
-                    print(
-                        "Invalid process creation details: %r" % details,
-                        file=sys.stderr,
-                    )
-                else:
-                    childpid, childcmd = m.groups()
-                    operations.append(('create-proc', childpid, childcmd))
+        if operation in IGNORED_OPERATIONS:
+            pass
+        elif operation == 'Load Image':
+            if result == 'SUCCESS':
+                self.operations.append(('read', path))
+        elif operation == 'CreateFile':
+            if result == 'SUCCESS':
+                info = parse_details(details)
+                mode = self.parse_access_mode(info['Desired Access'])
+                self.operations.append((mode, path))
+        elif operation == 'RegOpenKey':
+            pass  # TODO
+        elif operation == 'Process Create':
+            m = re.match(r'^PID: ([0-9]+), Command line: .*$', details)
+            if m is None:
+                print(
+                    "Invalid process creation details: %r" % details,
+                    file=sys.stderr,
+                )
             else:
-                unknown_operations[operation] += 1
-
-    if unknown_operations:
-        print(
-            "\nUnknown operations:\n%s" % '\n'.join(
-                '    %s (%d)' % p
-                for p in unknown_operations.items()
-            ),
-            file=sys.stderr,
-        )
-    if unknown_modes:
-        print(
-            "\nUnknown access modes:\n%s" % '\n'.join(
-                '    %s (%d)' % p
-                for p in unknown_modes.items()
-            ),
-            file=sys.stderr,
-        )
-
-    return operations
+                childpid, childcmd = m.groups()
+                self.operations.append(('create-proc', childpid, childcmd))
+        else:
+            self.unknown_operations[operation] += 1
 
 
 if __name__ == '__main__':
